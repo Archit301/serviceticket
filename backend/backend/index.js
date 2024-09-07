a8b2ea7c15a9009c6b32e10c588341f84c4bc7c8
Archit301 <114588710+Archit301@users.noreply.github.com>
2024-09-07 23:53:07 +0530

    Update index.js

--- backend/backend/index.js
+++ backend/backend/index.js
@@ -17,9 +17,9 @@ const app=express();
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
-
+const mongoURI = "mongodb+srv://tambiarchit:archit123@cluster0.6oq9wtf.mongodb.net/test?retryWrites=true&w=majority"
 mongoose
-    .connect(process.env.MONGO)
+    .connect(mongoURI)
     .then(()=>{
         console.log("Database is connected")
     })
